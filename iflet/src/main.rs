c53a51f391ad697305d02e238326c65bc1cbbd18
runnerlizard <48693102serjkeeE@>
2022-09-12 10:22:57 +0300

    templates

---
+++ iflet/src/main.rs
@@ -0,0 +1,57 @@
+fn if_let() {
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color{
+        println!("Using your favorite color, {color}, as as the background");
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
+}
+
+fn while_let() {
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{top}");
+    }
+}
+
+fn for_func() {
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{value} is at index {index}");
+    }
+}
+
+fn templates() {
+    let x = 1;
+
+    match x {
+        1..=4 => println!("one or four"),
+        52 => println!("fifty two"),
+        5 => println!("five"),
+        _ => println!("anything"),
+    }
+}
+
+fn main() {
+    // if_let();
+    // while_let();
+    // for_func();
+    templates();
+}
